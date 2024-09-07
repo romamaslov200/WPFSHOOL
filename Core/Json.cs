035be535f477fdbb452111dea6018f44691013ae
Roman <romanmaslov200@gmail.com>
2024-09-07 10:28:50 +0300

    Добавьте файлы проекта.

---
+++ Core/Json.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace WPFSHOOL.Core
+{
+    class Json
+    {
+        public bool ResizeMode { get; set; } = false;
+        public short Width { get; set; } = 800;
+        public short Height { get; set; } = 450;
+    }
+}
